30778384e7a1b88cc36cdde9a9929501e981cca4
Patricia Souza <patricia.sz.br@gmail.com>
2025-09-04 10:27:38 -0300

    return api - checkpoint

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello Binance");
+const axios = require("axios");
+
+setInterval(async () => {
+  const {data} = await axios.get("https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=5000");
+  
+  const bidsFloat = data.bids.map(b => [parseFloat(b[0]), parseFloat(b[1])]);
+  const asksFloat = data.asks.map(a => [parseFloat(a[0]), parseFloat(a[1])]);
+
+  // Calcula o valor nominal (notional)
+  const bidsNotional = bidsFloat.map( b => b[0] * b[1]);
+  const asksNotional = asksFloat.map( a => a[0] * a[1]);
+  
+  // Somatorio de valores nominais
+  const bidsSum = bidsNotional.reduce((a,b) => a + b);
+  const asksSum = asksNotional.reduce((a,b) => a + b);
+  
+  console.log("Birds:" + bidsSum);
+  console.log("Asks:" + asksSum);
+
+  let difference = 0;
+
+  if(bidsSum > asksSum) {
+    console.log("Força compradora vencendo, o preço tende a subir!");
+    difference = (bidsSum * 100 / asksSum) - 100;
+    
+  }
+  else {
+    console.log("Força vendedora vencendo, o preço tende a cair!");
+    difference = (asksSum * 100 / asksSum) - 100;
+  }
+  console.log(difference.toFixed(2) + "%");
+  
+},300)
